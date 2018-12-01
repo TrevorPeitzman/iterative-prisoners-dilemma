c86c23701df3aef16bd023c711a096df21895c7c
Brandon A. Rios <44213901+BrandonRios@users.noreply.github.com>
2018-11-30 16:03:43 -0800

    Comments & Attributions
    ^

--- team1.py
+++ team1.py
@@ -1,44 +1,24 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'Bscts&Grvy' # Only 10 chars displayed.
+team_name = 'Bscts&Grvy'
 strategy_name = 'Timmy the Trustworthy'
 strategy_description = 'Begins with collusion. Mimics the answer of the opponent in the previous round.'
     
-def move(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score): #Identifies all possible used variables in game.
     if len(their_history) == 0:
         return 'c'
+    #First Round: no response is given, so their_history == 0 characters. Program colludes as initial response. --Brandon Rios
     elif their_history[-1] == 'b':
         return 'b'
+    ##Subsequent Rounds: If opponent answered with 'betray' the previous round, respond with betray. --Brandon Rios
     else:
         return 'c'
-         
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-
+    ###Subsequent Rounds (cont'd): If opponent responded with other (a.k.a. Collude) in previous round, respond with collude. --Brandon Rios
+    ####
+    #   Variables 'my_history', 'my_score', and 'their_score' are not considered b/c Timmy the Trustworthy does not take them into
+    #   account when making his decision. The strategy is a simple one: mimic the answer of his opponent's from the previous round.
+    #   Using the psychology of human competition against them, for his own benefit.                        --Brandon Rios
+    ####
     
 def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
         return True
@@ -52,22 +32,15 @@ def test_move(my_history, their_history, my_score, their_score, result):
 
 if __name__ == '__main__':
      
-    # Test 1: Betray on first move.
     if test_move(my_history='',
               their_history='', 
               my_score=0,
               their_score=0,
               result='b'):
          print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
+     
     test_move(my_history='bbb',
               their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
               result='b')             
