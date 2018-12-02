09dc0044693fae4551c2260d3813708f23fe1bf1
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-12-01 21:30:18 -0800

    Lots More Dummy Strats
    Guessing roughly 50% of class will pick Tit4Tat

--- team8.py
+++ team8.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'T4T_6' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
@@ -18,15 +18,14 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(their_history) == 0:
+        return 'c'
+    #First Round: no response is given, so their_history == 0 characters. Program colludes as initial response. --Brandon Rios
+    elif their_history[-1] == 'b':
+        return 'b'
+    ##Subsequent Rounds: If opponent answered with 'betray' the previous round, respond with betray. --Brandon Rios
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
