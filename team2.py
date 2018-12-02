d9c7cb348fdfe790fd533c30680d154a5fecb1f2
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-12-01 18:27:44 -0800

    Optimistic Oscar - Just Code
    only optimistic 66% of the time. Plays CCB then repeats

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Optimistic Oscar' # Only 10 chars displayed.
+strategy_name = 'Cycler_CCB'
+strategy_description = 'wants love for all, 66% of the time'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +25,11 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+
+    if len(my_history) % 3 == 0:
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
